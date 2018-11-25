441f931f2b7e23f1abb5db82c272d33d462d6e5d
HyperEpsilon <6267244+HyperEpsilon@users.noreply.github.com>
2018-11-25 02:05:07 -0700

    Changed DB structure and added update_from_last
    comic_id and char_id are now added automatically upon insertion
    unix_time was added to speed up queries
    add_tag no longer relies upon exception catching
    separated comic scraping into it's own function

--- Housepets! Database.py
+++ Housepets! Database.py
@@ -2,7 +2,7 @@
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 from pprint import pprint
-import sqlite3, os
+import sqlite3, os, datetime
 
 connection = None
 cursor = None
@@ -21,31 +21,33 @@ def define_tables():
 
     comic_query=   '''
                         CREATE TABLE IF NOT EXISTS comic (
-                                    comic_id INT,
+                                    comic_id INTEGER PRIMARY KEY,
                                     date DATE,
+                                    unix_date INT,
                                     title TEXT,
                                     alt_text TEXT,
                                     url TEXT,
-                                    next_comic TEXT,
-                                    PRIMARY KEY (comic_id)
+                                    next_comic TEXT
                                     );
                     '''
 
     character_query=  '''
                         CREATE TABLE IF NOT EXISTS  character (
+                                    char_id INTEGER PRIMARY KEY,
                                     name TEXT,
                                     species TEXT,
-                                    PRIMARY KEY (name)
+                                    notes TEXT
                                     );
                     '''
 
     tag_query= '''
                     CREATE TABLE IF NOT EXISTS tag (
-                                comic_id TEXT,
+                                comic_id INT,
+                                char_id INT,
                                 name TEXT,
-                                PRIMARY KEY (comic_id, name),
+                                PRIMARY KEY (comic_id, char_id),
                                 FOREIGN KEY(comic_id) REFERENCES comic(comic_id) ON DELETE CASCADE,
-                                FOREIGN KEY(name) REFERENCES character(name) ON DELETE CASCADE
+                                FOREIGN KEY(char_id) REFERENCES character(char_id) ON DELETE CASCADE
                                 );
                 '''
     
@@ -58,10 +60,10 @@ def define_tables():
 
     return
 
-
-def drop_tables():
+# Defines which tables to drop from the DB upon recreation
+# Character is often excluded because there's a lot of manual entry
+def drop_tables(*drop_tables):
     global connection, cursor
-    drop_tables = ['tag', 'character', 'comic']
     for table in drop_tables:
         cursor.execute('drop table if exists {}'.format(table))    
 
@@ -69,24 +71,15 @@ def make_soup(comic_page):
     page = urlopen(comic_page)
     return BeautifulSoup(page, 'html.parser')    
 
-def get_comic_id(soup):
-    ## Extract the comic ID from the 'comic-wrap' div's class,
-    ## then return it as an int
-    
-    c_id = soup.find('div', attrs = {'id':'comic-wrap'})['class'][0]
-    c_id = c_id.split('-')[2]
-    c_id = int(c_id)
-    
-    return c_id
-
 def get_date(soup):
     ## Extract the comic date from the comic URL, then convert it
-    ## into an SQL compatible date format
+    ## into an SQL compatible date format and unix timestamp
     
     date = soup.find('link', attrs = {'rel':'canonical'})['href']
     date = '-'.join(date.split('/')[4:7])
+    unix_date = int(datetime.datetime.strptime(date, "%Y-%m-%d").replace(tzinfo=datetime.timezone.utc).timestamp())
     
-    return date
+    return date, unix_date
 
 def get_title(soup):
     ## Extract the comic title from the title header
@@ -118,62 +111,66 @@ def get_tags(soup):
     
     return tags
 
-def add_comic(comic_id, date, title, alt_text, url, next_comic):
-    data = (comic_id, date, title, alt_text, url, next_comic)
-    
-    try:
-        cursor.execute('INSERT INTO comic (comic_id, date, title, alt_text, url, next_comic) VALUES (?,?,?,?,?,?)', data)
-    except sqlite3.IntegrityError:
-        pass    
+def add_comic(date, unix_date, title, alt_text, url, next_comic):
+    data = [date, unix_date, title, alt_text, url, next_comic]
+
+    # Checks if the comic url is already in the database, adds it if it's not
+    # and updates it if it is. It then returns whether to add tags for the comic
+    if not cursor.execute("SELECT * FROM comic WHERE url=?", (url,)).fetchall():
+        cursor.execute('INSERT INTO comic (date, unix_date, title, alt_text, url, next_comic) VALUES (?,?,?,?,?,?)', data)
+        return True
+    else:
+        cursor.execute("UPDATE comic SET date = ?, unix_date = ?, title = ?, alt_text = ?, url = ?, next_comic = ? WHERE url = ?", data + [url])
+        return False
+
 
 def add_tag(comic_id, tag):
-    try:
-        cursor.execute('INSERT INTO tag (comic_id, name) VALUES (?,?)', (comic_id, tag))
-    except sqlite3.IntegrityError as inst:
-        if str(inst.args) == "('FOREIGN KEY constraint failed',)":
-            cursor.execute('INSERT INTO character (name) VALUES (?)', (tag,))
-            cursor.execute('INSERT INTO tag (comic_id, name) VALUES (?,?)', (comic_id, tag))
-        else:
-            print(inst.args)
-            raise
+    char_id = cursor.execute("SELECT char_id FROM character WHERE name=?", (tag,)).fetchall()
+    
+    if not len(char_id):
+        cursor.execute('INSERT INTO character (name) VALUES (?)', (tag,))
+        char_id = int(cursor.lastrowid)
+    else:
+        char_id = char_id[0][0]
+    
+    cursor.execute('INSERT INTO tag (comic_id, char_id, name) VALUES (?,?,?)', (comic_id, char_id, tag))
+
+def scrape(comic_page):
+    while comic_page:
+    #for i in range(10):
+        soup = make_soup(comic_page)
+        
+        date, unix_date = get_date(soup)
+        title = get_title(soup)
+        alt_text = get_alt_text(soup)
+        next_comic = get_next_comic(soup)
+        
+        add_tags = add_comic(date, unix_date, title, alt_text, comic_page, next_comic)
+        comic_id = cursor.lastrowid
+        
+        if add_tags:
+            tags = get_tags(soup)
+            for tag in tags:
+                add_tag(comic_id, tag)
+        
+        print(title)
+        
+        comic_page = next_comic    
 
 def scrape_from_beginning():
     global connection, cursor
     
-    # Implement method of grabing comic with next comic as null for start point
-    # Also update the old row to include the new data
+    path="./Housepets.db"
+    connect(path)    
+    
     comic_page = "http://www.housepetscomic.com/comic/2008/06/02/when-boredom-strikes/"
     
     try:
-        path="./Housepets.db"
-        connect(path)
-        #connection.create_function('GPA', 1, GPA)
-    
-        #drop_tables()
+        #drop_tables('tag', 'comic', 'character')
+        drop_tables('tag', 'comic')
         define_tables()        
         
-        while comic_page:
-        #for i in range(10):
-            soup = make_soup(comic_page)
-            
-            comic_id = get_comic_id(soup)
-            date = get_date(soup)
-            title = get_title(soup)
-            alt_text = get_alt_text(soup)
-            next_comic = get_next_comic(soup)
-            
-            add_comic(comic_id, date, title, alt_text, comic_page, next_comic)
-            
-            tags = get_tags(soup)
-            
-            for tag in tags:
-                add_tag(comic_id, tag)
-    
-            #pprint(tags)
-            
-            print(title)
-            
-            comic_page = next_comic
+        scrape(comic_page)
     
     except KeyboardInterrupt:
         pass
@@ -185,10 +182,33 @@ def scrape_from_beginning():
     return
 
 def update_from_last():
-    pass
+    global connection, cursor
+    
+    path="./Housepets.db"
+    connect(path)    
+    
+    try:
+        comic_page = cursor.execute("select url FROM comic WHERE next_comic is Null").fetchall()[0][0]
+    except (sqlite3.OperationalError, IndexError):
+        scrape_from_beginning()
+        return
+    
+    try:
+        scrape(comic_page)
+    
+    finally:
+        connection.commit()
+        connection.close()    
+
+def restore_character_data():
+    backup = cursor.execute('''SELECT species, notes, name FROM character_backup''').fetchall()
+    
+    query = '''UPDATE character SET species = ?, notes = ? WHERE name = ?'''
+    cursor.executemany(query, backup)
+    connection.commit()
+    connection.close()
 
 def main():
-    
     while True:
         option = -1
         confirmed = ""
@@ -207,24 +227,20 @@ def main():
     
         if option == 0:
             return
+        
         elif option == 1:
             while confirmed not in ('Y','N'):
                 confirmed = input ("This will overwrite the current database. Are you sure? (Y/N) ").upper()
             os.system('cls')
             
             if confirmed == 'Y':
-                #scrape_from_beginning()
-                #return
-                print("Yes")
+                scrape_from_beginning()
+                return
+        
         elif option == 2:
-            while confirmed not in ('Y','N'):
-                confirmed = input ("Are you sure? (Y/N) ").upper()
-            os.system('cls')
-            
-            if confirmed == 'Y':
-                #update_from_last()
-                #return
-                print("Yes")            
+            os.system('cls')            
+            update_from_last()
+            return
 
 if __name__ == "__main__":
     main()
